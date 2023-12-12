a8e9067e93e563b01cfad33a05900c2481c2763c
sinhakunal749 <kunalsinha749@gmail.com>
2023-12-12 17:37:49 +0530

    main

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,30 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection=null;
+    public static Connection getConnection(){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/myproject?useSSL=false","root","kunal");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("connection successfull created");
+        return connection;
+    }
+    public static void closeConnection(){
+        if(connection!=null){
+            try{
+                connection.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
